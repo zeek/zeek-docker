6c405e8314678feb83b17d0db5a3fb6df0e5cba7
Justin Azoff <justin@bouncybouncy.net>
2015-01-23 19:46:29 -0500

    first working version

--- Makefile
+++ Makefile
@@ -1,3 +1,5 @@
 all:
 	$(MAKE) -C build
+
+	cp -al common 2.3.1
 	$(MAKE) -C 2.3.1
--- common/getgeo.sh
+++ common/getgeo.sh
@@ -4,10 +4,12 @@ echo "2015-01-23"
 wget -N http://geolite.maxmind.com/download/geoip/database/GeoLiteCity.dat.gz
 zcat GeoLiteCity.dat.gz > GeoIP.dat.new
 mv GeoIP.dat.new /usr/share/GeoIP/GeoIPCity.dat
+rm GeoLiteCity.dat.gz
 
 wget -N http://geolite.maxmind.com/download/geoip/database/GeoLiteCityv6-beta/GeoLiteCityv6.dat.gz
 zcat GeoLiteCityv6.dat.gz > GeoIPCityv6.dat
 mv GeoIPCityv6.dat /usr/share/GeoIP/GeoIPCityv6.dat
+rm GeoLiteCityv6.dat.gz
 
 #wget -N http://download.maxmind.com/download/geoip/database/asnum/GeoIPASNum.dat.gz
 #zcat GeoIPASNum.dat.gz > GeoIPASNum.dat
